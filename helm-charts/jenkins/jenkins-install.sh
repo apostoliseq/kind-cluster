ca279569156e568123b8dc02f988507af2ce62a1
apostoliseq <apostolisn@hotmail.com>
2025-07-02 14:19:58 +0000

    Refactor Jenkins chart

--- helm-charts/jenkins/jenkins-install.sh
+++ helm-charts/jenkins/jenkins-install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 JENKINS_HOME=~/workspace/kind-cluster/jenkins
-JENKINS_KUBERNETES=~/workspace/kind-cluster/jenkins/kubernetes
 
-kubectl apply -f $JENKINS_KUBERNETES/namespace.yaml
-helm install jenkins $JENKINS_HOME/helm-chart/ -n jenkins -f $JENKINS_KUBERNETES/values.yaml
-kubectl apply -f $JENKINS_KUBERNETES/rbac.yaml
+helm upgrade --install jenkins \
+    $JENKINS_HOME/helm-chart/ \
+    --values $JENKINS_HOME/values.yaml \
+    --atomic \
+    --cleanup-on-fail \
+    --timeout 10m
