80be4d488f7437f212dfb262112335e5cd3388db
apostoliseq <apostolisn@hotmail.com>
2025-07-29 00:34:15 +0300

    Use secrets for admin & nexus admin credentials

--- helm-charts/jenkins/jenkins-install.sh
+++ helm-charts/jenkins/jenkins-install.sh
@@ -3,13 +3,42 @@
 HELM_CHARTS_HOME=/home/$USER/workspace/kind-cluster/helm-charts
 JENKINS_HOME=$HELM_CHARTS_HOME/jenkins
 
+NAMESPACE="jenkins"
+
+JENKINS_ADMIN_SECRET="jenkins-admin-secret"
+JENKINS_ENV_FILE="$JENKINS_HOME/jenkins.env"
+
+NEXUS_ADMIN_SECRET="nexus-admin-secret"
+NEXUS_ENV_FILE="$JENKINS_HOME/nexus.env"
+
+# Check if secret exists
+if ! kubectl get secret $JENKINS_ADMIN_SECRET -n $NAMESPACE >/dev/null 2>&1; then
+    echo "Creating jenkins-admin secret..."
+    
+    kubectl create secret generic $JENKINS_ADMIN_SECRET \
+        --from-env-file=$JENKINS_ENV_FILE \
+        --namespace $NAMESPACE
+else
+    echo "Secret $JENKINS_ADMIN_SECRET already exists"
+fi
+
+if ! kubectl get secret $NEXUS_ADMIN_SECRET -n $NAMESPACE >/dev/null 2>&1; then
+    echo "Creating nexus-admin secret..."
+    
+    kubectl create secret generic $NEXUS_ADMIN_SECRET \
+        --from-env-file=$NEXUS_ENV_FILE \
+        --namespace $NAMESPACE
+else
+    echo "Secret $NEXUS_ADMIN_SECRET already exists"
+fi
+
 helm upgrade --install jenkins \
     $JENKINS_HOME/helm-chart/ \
-    --namespace jenkins \
+    --namespace $NAMESPACE \
     --create-namespace \
     --values $JENKINS_HOME/helm-chart/values.yaml \
     --values $JENKINS_HOME/helm-chart/values-override.yaml \
     --atomic \
     --cleanup-on-fail \
-    --timeout 10m \
+    --timeout 5m \
     --debug
